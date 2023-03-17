dcc97fcf87b62306d0c67c710fc57e9a15266289
reome <shub29@naver.com>
2023-03-17 19:50:11 +0900

    operators, methods
    c# operators, methods basic

--- CSharp/CSharpBasic/Operators/Program.cs
+++ CSharp/CSharpBasic/Operators/Program.cs
@@ -1,9 +1,9 @@
 ﻿//right 값을 left 값에 대입
 //R-Value : 값을 참조하기 위한 (읽기위한) 값
 //L-Value : 값을 대입하기 위한 (쓰기위한) 값
-float a = 14;
-float b = 6;
-float c = 0;
+int a = 14;
+int b = 6;
+int c = 0;
 
 //산술 연산자
 //더하기, 빼기, 곱하기, 나누기, 나머지
@@ -77,3 +77,68 @@ c -= b;
 c *= b;
 c /= b;
 c %= b;
+
+//논리 연산자
+//논리형의 피연산자들을 대상으로 연산을 수행
+//or, and, xor, not
+//==========================================
+Console.WriteLine("======논리연산======");
+bool A = true;
+bool B = true;
+
+//or
+//A와 B 둘 중 하나라도 true이면 true, 아니면 false
+Console.WriteLine(A | B);
+
+//and
+//A와 B 둘 다 true이면 true, 아니면 false
+Console.WriteLine(A & B);
+
+//xor
+//A와 B 둘중 하나만 true면 true(다르면 true 같으면 false)
+Console.WriteLine(A ^ B);
+
+//not
+//피연산자가 true면 false
+Console.WriteLine(!A);
+
+//조건부 논리연산자
+//Conditional or, Conditional and
+//===========================================
+
+//Conditional or -> A가 true면 B가 뭐든 결과는 true -> B 안읽음
+Console.WriteLine(A || B);
+
+//Conditional and -> A가 false면 B가 뭐든 결과는 false -> B 안읽음
+Console.WriteLine(A && B);
+
+//비트 연산자
+//정수형에 대해서만 연산을 수행함
+//==========================================
+
+
+//or
+//a == 14 == 00001110 (앞에 0 생략)
+//b == 6 ==  00000110
+Console.WriteLine(a | b);
+
+//and
+Console.WriteLine(a & b);
+
+//xor
+Console.WriteLine(a^b);
+
+//not
+//00000000 00000000 00000000 00001110
+//11111111 11111111 11111111 11110001
+Console.WriteLine(~a);
+
+//2의 보수 : 이진법에서 모든 자릿수를 반전하고 +1, ~a + 1
+//2의 보수의 2의 보수하면 다시 양수 뿅
+
+//shift - left
+Console.WriteLine(a << 1);
+
+//shift - right
+//00001110 -> 00000011 범위 넘어가면 날아감
+Console.WriteLine(a >> 2);
