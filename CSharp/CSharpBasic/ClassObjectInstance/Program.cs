efdabd273a9886fe09d6167df08c708e406deaaa
reome <shub29@naver.com>
2023-03-21 19:50:00 +0900

    c# oop basic
    c# oop basic
    class

--- CSharp/CSharpBasic/ClassObjectInstance/Program.cs
+++ CSharp/CSharpBasic/ClassObjectInstance/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿using ClassObjectInstance;
+
+//new 키워드 : 동적할당키워드
+//메인에서 만든거염
+SwordMan swordMan1 = new SwordMan();
+SwordMan swordMan2 = new SwordMan();
+int a = 3;
+
+//멤버접근연산자
+swordMan1.Name = "짱짱";
+swordMan1.Lv = 1;
+
+swordMan2.Name = "똠양꿍";
+swordMan2.Lv = 2;
+
+swordMan1.Slash();
+swordMan2.Slash();
+
+//함수 호출 시 함수 내용 코드 전체 복사해서 스택에 쌓임
---
+++ CSharp/CSharpBasic/Structure/SwordMan.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structure
+{
+    internal struct SwordMan
+    {
+        //멤버 변수
+        String _name;
+        int _lv;
+        float _exp;
+        char _gender;
+
+        //멤버 함수
+        void Slash()
+        {
+            Console.WriteLine($"{_name} 이(가) 베기를 시전했다.");
+        }
+    }
+}
