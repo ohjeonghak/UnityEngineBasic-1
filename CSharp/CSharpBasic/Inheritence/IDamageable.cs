7ae1c3a160a3f5668d1034c51108b47b6d2c58f6
reome <shub29@naver.com>
2023-03-31 19:50:36 +0900

    c# inheritance2
    c# inheritance, override, overload

--- CSharp/CSharpBasic/Inheritence/IDamageable.cs
+++ CSharp/CSharpBasic/Inheritence/IDamageable.cs
@@ -7,10 +7,10 @@ using System.Threading.Tasks;
 namespace Inheritance
 {
     //interface는 디폴트 접근제한자가 public
-    //멤버들을 추상화하기위한 용도이므로 기본적으로 멤버들 선언만 함
+    //프로퍼티, 함수, 이벤트 멤버들을 추상화하기위한 용도이므로 기본적으로 멤버들 선언만 함
     internal interface IDamageable
     {
         int Hp { get; }
-        void Damage(int value);
+        void Damage(IAttackable attacker, int value);
     }
 }
--- CSharp/CSharpBasic/Inheritence/Program.cs
+++ CSharp/CSharpBasic/Inheritence/Program.cs
@@ -1,8 +1,8 @@
-﻿//interface 프로퍼티 함수 이벤트끼리 있을 때 만들 수 있음
-using Inheritance;
+﻿using Inheritance;
 
-Knight knight = new Knight();
-Goblin goblin = new Goblin();
+Knight knight = new Knight(200, 50);
+Wizard wizard = new Wizard(150, 70);
+Goblin goblin = new Goblin(100, 20);
 
 //공변성(Covariance)
 //하위타입을 기반타입으로 참조가 가능한 성질 (기반이 상위에 있는 애) 추상클래스 인터페이스 등에서 성립
@@ -15,4 +15,12 @@ for (int i = 0; i < creatures.Length; i++)
     Console.WriteLine(creatures[i].Lv);
 }
 
+ 
 knight.Attack(goblin);
+
+knight.SayHi();
+wizard.SayHi();
+Character character1 = knight;
+Character character2 = wizard;
+character1.SayHi();
+character2.SayHi();
