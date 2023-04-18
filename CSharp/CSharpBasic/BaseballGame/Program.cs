2a1fdadf7c39ab98c155c698a6bd6acf8934e10d
reome <shub29@naver.com>
2023-04-18 19:51:32 +0900

    baseball edit, cardgame
    baseball edit, cardgame start

--- CSharp/CSharpBasic/BaseballGame/Program.cs
+++ CSharp/CSharpBasic/BaseballGame/Program.cs
@@ -1,10 +1,6 @@
-﻿int num = 832;
-string numTostring = num.ToString();
+﻿using System;
 
-Console.WriteLine("컴퓨터가 값을 생각했어요");
-
-//string input = Console.ReadLine();
-string input = "425";
+//string input = "425";
 
 //Console.WriteLine("input의 값은 : " + input);
 //Console.WriteLine("num의 타입은 : " + num.GetType);
@@ -13,36 +9,70 @@ string input = "425";
 //Console.WriteLine(input[0]);
 //Console.WriteLine(numTostring[0]);
 
-//if (input[0] == numTostring[0])
-//{
-//    Console.WriteLine("둘이 값이 같습니다.");
-//}
-//else
-//{
-//    Console.WriteLine("달라요.");
-//}
+int num = 832;
+string numToString = num.ToString();
 
 int count = 10;
-string input2;
+string input;
 
-//for j에서 다 다르면 out 1
+Random random= new Random();
+int randomNum = random.Next();
+string answer = randomNum.ToString();
 
+//for(int i = 0; i < answer.Length; i++)
+//{
+
+//}
+
+//int[] answerArray = new int[3];
+//for(int i = 0; i < answerArray.Length;i++)
+//{
+//    randomNum = random.Next(0,10);
+//    answerArray[i] = randomNum;
+  
+//    for(int j = 0; j<i;j++)
+//    {
+//        if(answerArray[i] == answerArray[j])
+//        {
+//            i--;
+//        }
+//    } 
+//}
+
+//for(int i = 0; i<answerArray.Length;i++)
+//{
+//    Console.Write(answerArray[i]);
+//}
+
+//Console.WriteLine(answer);
+
+Console.WriteLine("컴퓨터가 값을 생각했어요");
+Console.WriteLine();
+
+//게임 루프
 while(count > 0)
 {
     Console.Write("값을 입력하세요 : ");
-    input2 = Console.ReadLine();
+    input = Console.ReadLine();
 
+    //int[] inputArray = input.ToArray<int>;
+
+    //for (int i = 0; i < inputArray.Length; i++)
+    //{
+    //    Console.Write(inputArray[i]);
+    //    Console.WriteLine();
+    //}
 
     int strikes = 0;
     int balls = 0;
     int outs = 0;
     int none = 0;
 
-    for (int i = 0; i < input2.Length; i++)
+    for (int i = 0; i < input.Length; i++)
     {
-        for (int j = 0; j < numTostring.Length; j++)
+        for (int j = 0; j < answer.Length; j++)
         {
-            if (input2[i] == numTostring[j])
+            if (input[i] == answer[j])
             {
                 if (i == j)
                 {
@@ -54,42 +84,39 @@ while(count > 0)
                     balls++;
                     continue;
                 }
-
             }
             else
             {
                 none++;
             }
-
         }
+        //for j에서 다 다르면 out 1
         if (none == 3)
         {
             outs++;
         }
         none = 0;
-
     }
 
     Console.WriteLine($"{strikes} 스트라이크");
     Console.WriteLine($"{balls} 볼");
     Console.WriteLine($"{outs} 아웃");
    
-
     if (strikes == 3)
     {
         Console.WriteLine();
-        Console.WriteLine("축하합니다. 이겼습니다.");
+        Console.WriteLine("정답입니다. 이겼습니다.");
         return;
     }
 
     count--;
     Console.WriteLine($"남은 기회 : {count}회");
     Console.WriteLine();
-    Console.WriteLine();
 }
 
 if(count == 0)
 {
     Console.WriteLine("남은 기회가 없습니다. 졌습니다. ");
+    Console.WriteLine($"정답은 {answer} 이었습니다.");
 }
 
---
+++ CSharp/CSharpBasic/CardGame/Program.cs
@@ -0,0 +1,20 @@
+﻿// 카드 A(6으로 저장) 7 8 9 10 J(11) Q(12) K(13)
+// 클로버(c), 다이아(d), 하트(h), 스페이드(s)
+//                0      1      2      3      4      5      6      7
+String[] dec = {"06c", "07c", "08c", "09c", "10c", "11c", "12c", "13c",
+                "06d", "07d", "08d", "09d", "10d", "11d", "12d", "13d",
+                "06h", "07h", "08h", "09h", "10h", "11h", "12h", "13h",
+                "06s", "07s", "08s", "09s", "10s", "11s", "12s", "13s"};
+
+List<String> myCard = new List<String>();
+
+myCard.Add(dec[0]); //06c
+myCard.Add(dec[8]); //06d
+myCard.Add(dec[9]); //07d
+myCard.Add(dec[10]); //08d
+myCard.Add(dec[11]); //09d
+
+Console.WriteLine(myCard[0].Substring(0,2));  // 문자열 나누기 : Substring(start index, length)
+
+int a = Convert.ToInt32(myCard[0].Substring(0, 2));
+
