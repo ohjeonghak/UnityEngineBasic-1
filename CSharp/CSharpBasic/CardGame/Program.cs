66ffaa1e521246789b14c518f12a3343e2ce9d40
reome <shub29@naver.com>
2023-04-21 19:52:04 +0900

    card game edit
    변경 기능 추가

--- CSharp/CSharpBasic/CardGame/Program.cs
+++ CSharp/CSharpBasic/CardGame/Program.cs
@@ -1,175 +1,190 @@
 ﻿// 카드 A(6으로 저장) 7 8 9 10 J(11) Q(12) K(13)
 // 클로버(c), 다이아(d), 하트(h), 스페이드(s)
 //                0      1      2      3      4      5      6      7
-String[] deck = {"06c", "07c", "08c", "09c", "10c", "11c", "12c", "13c",
+string[] deck = {"06c", "07c", "08c", "09c", "10c", "11c", "12c", "13c",
                 "06d", "07d", "08d", "09d", "10d", "11d", "12d", "13d",
                 "06h", "07h", "08h", "09h", "10h", "11h", "12h", "13h",
                 "06s", "07s", "08s", "09s", "10s", "11s", "12s", "13s"};
 
-List<String> myCard = new List<String>();
+List<string> myCard = new List<string>();
 List<int> myCardOnlyNum = new List<int>(); //숫자만
 List<string> myCardOnlyShape = new List<string>(); //모양만
+List<int> cardIndex = new List<int>(); //랜덤으로 뽑은 인덱스 저장
 
-myCard.Add(deck[0]);
-myCard.Add(deck[9]); 
-myCard.Add(deck[3]); 
-myCard.Add(deck[5]); 
-myCard.Add(deck[15]); 
+Random random= new Random();
+int index;
+for (int i = 0; i < 6;)
+{
+    index = random.Next(0, deck.Length);
+    if (!cardIndex.Contains(index))
+    {
+        cardIndex.Add(index);
+        i++;
+    }
+}
+
+//myCard에 카드 넣기
+for (int i = 0; i < 5; i++)
+{
+    myCard.Add(deck[cardIndex[i]]);
+}
 
 //Console.WriteLine(myCard[0].Substring(0,2));  // 문자열 나누기 : Substring(start index, length)
 
-//숫자와 모양 나누어서 저장
-for (int i = 0; i < myCard.Count; i++)
+//카드 출력하고 바꿀지말지 선택 후 비교하고 종료
+
+gameStart();
+
+void gameStart()
 {
-    myCardOnlyNum.Add(Convert.ToInt32(myCard[i].Substring(0, 2)));
-    myCardOnlyShape.Add(myCard[i].Substring(2, 1));
-}
+    bool isFlush = false;
+    bool isMount = false;
+    bool isStraight = false;
+    int pair = 0;
 
-//카드 출력
-for (int i = 0; i < myCard.Count; i++)
-{
-    Console.Write(myCard[i] + " ");
-}
-Console.WriteLine();
-Console.WriteLine();
-
-//모양 정렬
-myCardOnlyShape.Sort();
-
-//플러쉬 확인
-bool isFlush = false;
-
-if (myCardOnlyShape[0] == myCardOnlyShape[myCardOnlyShape.Count-1])
-{
-    isFlush = true;
-    Console.WriteLine("플러쉬입니다.");
-}
-else
-{
-    Console.WriteLine("플러쉬가 아닙니다.");
-}
-
-//숫자 정렬
-myCardOnlyNum.Sort();
-
-//마운틴 확인
-bool isMount = false;
-
-if (myCardOnlyNum[0]==06 && myCardOnlyNum[1]==10 && myCardOnlyNum[2]==11 && myCardOnlyNum[3]==12 && myCardOnlyNum[4]==13)
-{
-    isMount= true;
-    Console.WriteLine("마운틴입니다.");
-}
-else
-{
-    Console.WriteLine("마운틴이 아닙니다.");
-}
-
-//스트레이트 확인
-//count를 해서 4가 되면 스트레이트로 판단하는 방법도 있음
-bool isStraight = false;
-
-for (int i = 1; i < myCardOnlyNum.Count; i++)
-{
-    if (myCardOnlyNum[i-1] + 1 == myCardOnlyNum[i])
+    //숫자와 모양 나누어서 저장
+    for (int i = 0; i < myCard.Count; i++)
     {
-        isStraight = true;
+        myCardOnlyNum.Add(Convert.ToInt32(myCard[i].Substring(0, 2)));
+        myCardOnlyShape.Add(myCard[i].Substring(2, 1));
+    }
+
+    //카드 출력
+    Console.WriteLine("나의 카드는");
+    for (int i = 0; i < myCard.Count; i++)
+    {
+        Console.Write(myCard[i] + " ");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+
+    Console.WriteLine("카드를 변경하려면 c, 그대로 진행하려면 g를 입력하세요.");
+    string key = Console.ReadLine();
+    while (true)
+    {
+        if (key == "c")
+        {
+            changeCard();
+            break;
+        }
+        else if (key == "g")
+            break;
+        else
+        {
+            Console.WriteLine("잘못된 입력입니다. c와 g 중에서 입력하세요.");
+            continue;
+        }
+    }
+    Console.WriteLine();
+
+    //모양 정렬
+    myCardOnlyShape.Sort();
+    //숫자 정렬
+    myCardOnlyNum.Sort();
+
+    //플러쉬 확인
+    if (myCardOnlyShape[0] == myCardOnlyShape[myCardOnlyShape.Count - 1])
+    {
+        isFlush = true;
+        Console.WriteLine("플러쉬입니다.");
+    }
+
+    //마운틴 확인
+    if (myCardOnlyNum[0] == 06 && myCardOnlyNum[1] == 10 && myCardOnlyNum[2] == 11 && myCardOnlyNum[3] == 12 && myCardOnlyNum[4] == 13)
+    {
+        isMount = true;
+        Console.WriteLine("마운틴입니다.");
+    }
+
+    //스트레이트 확인
+    //count를 해서 4가 되면 스트레이트로 판단하는 방법도 있음
+    for (int i = 1; i < myCardOnlyNum.Count; i++)
+    {
+        if (myCardOnlyNum[i - 1] + 1 == myCardOnlyNum[i])
+        {
+            isStraight = true;
+            Console.WriteLine("스트레이트입니다.");
+        }
+        else
+        {
+            isStraight = false;
+            break;
+        }
+    }
+
+    //페어 확인
+    for (int i = 0; i < myCardOnlyNum.Count; i++)
+    {
+        for (int j = i + 1; j < myCardOnlyNum.Count; j++)
+        {
+            if (myCardOnlyNum[i] == myCardOnlyNum[j])
+            {
+                pair++;
+            }
+        }
+    }
+
+    //족보 완성 여부 체크
+    if (isMount && isFlush)
+    {
+        Console.WriteLine("로얄 스트레이트 플러쉬입니다.");
+    }
+    else if (isFlush && isStraight)
+    {
+        Console.WriteLine("스트레이트 플러쉬입니다.");
+    }
+    else if (pair == 6)
+    {
+        Console.WriteLine("포커입니다.");
+    }
+    else if (pair == 4)
+    {
+        Console.WriteLine("풀하우스입니다.");
+    }
+    else if (pair == 3)
+    {
+        Console.WriteLine("트리플입니다.");
+    }
+    else if (pair == 2)
+    {
+        Console.WriteLine("투페어입니다.");
+    }
+    else if (pair == 1)
+    {
+        Console.WriteLine("원페어입니다.");
     }
     else
     {
-        isStraight= false;
-        break;
+        Console.WriteLine("족보를 완성하지 못했습니다.");
     }
 }
-
-if(isStraight)
-{
-    Console.WriteLine("스트레이트입니다.");
-}
-else
-{
-    Console.WriteLine("스트레이트가 아닙니다.");
-}
-
-//스트레이트 플러쉬 확인
-bool isStFlush = false;
-if(isStraight && isFlush)
-{
-    isStFlush = true;
-    Console.WriteLine("스트레이트 플러쉬입니다.");
-}
-
-//페어 확인
-int pair = 0;
-
-for (int i = 0; i < myCardOnlyNum.Count; i++)
-{
-    for (int j = i+1; j < myCardOnlyNum.Count; j++)
-    {
-        if (myCardOnlyNum[i] == myCardOnlyNum[j])
-        {
-            pair++;
-        }
-    }
-}
-
-switch (pair)
-{
-    case 1:
-        Console.WriteLine("원페어입니다.");
-        break;
-    case 2:
-        Console.WriteLine("투페어입니다.");
-        break;
-    case 3:
-        Console.WriteLine("트리플입니다.");
-        break;
-    case 4:
-        Console.WriteLine("풀하우스입니다.");
-        break;
-    case 6:
-        Console.WriteLine("포커입니다.");
-        break;
-    default:
-        break;
-}
-
-//로얄 스트레이트 플러쉬
-bool isLoyal = false;
-if(isFlush && isMount)
-{
-    isLoyal= true;
-    Console.WriteLine("로얄 스트레이트 플러쉬입니다.");
-}
-
-if(pair==0 || isFlush==false || isStraight == false || isMount == false)
-{
-    Console.WriteLine();
-    Console.WriteLine("족보를 완성하지 못했습니다.");
-    Console.WriteLine($"가장 높은 카드는 {myCardOnlyNum[myCardOnlyNum.Count - 1]}{myCardOnlyShape[myCardOnlyShape.Count-1]} 입니다.");
-}
-
-//c 누르면 카드 바꾸는 함수 호출
-//게임 종료와 카드 변경 선택하기
-//카드 변경하면 변경하고 로직 다시 첨부터 실행
-
-int change = 3;
 
 void changeCard()
 {
-    if(change <= 0)
-    {
-        Console.WriteLine("카드 변경 기회를 전부 사용했습니다.");
-    }
-
     Console.WriteLine("변경하고 싶은 카드의 인덱스를 입력하세요.");
-    int input = Convert.ToInt32(Console.ReadLine());   
-
-    Random random = new Random();
-    int num = random.Next(0, deck.Length);
+    int input = Convert.ToInt32(Console.ReadLine());
    
     myCard.RemoveAt(input);
-    myCard.Add(deck[num]);
+    myCard.Add(deck[cardIndex[cardIndex.Count-1]]);
 
-    change--;
+    myCardOnlyNum.Clear();
+    myCardOnlyShape.Clear();
+    //for (int i = 0; i < myCard.Count; i++)
+    //{
+    //    myCardOnlyShape.RemoveAt(0);
+    //}
+
+    //숫자와 모양 나누어서 저장
+    for (int i = 0; i < myCard.Count; i++)
+    {
+        myCardOnlyNum.Add(Convert.ToInt32(myCard[i].Substring(0, 2)));
+        myCardOnlyShape.Add(myCard[i].Substring(2, 1));
+    }
+
+    Console.WriteLine("새로운 나의 카드는");
+    for (int i = 0; i < myCard.Count; i++)
+    {
+        Console.Write(myCard[i] + " ");
+    }
+    Console.WriteLine();
 }
