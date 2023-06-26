d83dd7c79fbd5e5e766e44414b09c531a9f06cd4
ohjeonghak <136331677+ohjeonghak@users.noreply.github.com>
2023-06-26 21:58:17 +0900

    Ingeritance3
    06.26
    
    Ingeritance 3th
    IDamageable, HpBar, Enemy
    
    delegate, event, property

---
+++ Csharp_JH/Inheritance/Character.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Inheritance
+{
+    // 추상멤버를 가지고있다
+    // -> 해당 멤버를 가지는 클래스가 인스턴스화 가능하다면 ㅎ해당 함수를 호출할 수 있어야하는데 모순이 생긴다.
+    // -> 추상멤버를 가지고 있으면, 클래스 마찬가지로 추상 클래스여야한다.
+    internal abstract class Character : Creature, IDamageable
+    {
+        // 프로퍼티 
+        public float hp
+        {
+            get
+            {
+                return _hp;
+            }
+            set
+            {
+                if (value < 0)
+                    value = 0;
+              
+                if (_hp == value)
+                    return;
+                
+                _hp = value;
+
+                //if (onHpChanged != null)
+                //    onHpChanged(value); // 직접호출 , 결과이상이 생길수도 있음
+                //
+                //onHpChanged.Invoke(value); // 간접호출 , 결과보장이됨
+                onHpChanged?.Invoke(value); // ? : Null check 연산자
+            }
+        }
+
+        private float _hp;
+        public delegate void HpChangedHandler(float value); // delegate : 대리자
+        public event HpChangedHandler onHpChanged; 
+        // event : 한정자
+        // 대리자의 접근 제한을 위한 한정자
+        // +=, -= (구독 / 구독취소) 는 외부 클래스에서 호출 가능.
+        // = 는 접근 불가능
+
+     
+
+        //public float GetHp()
+        //{
+        //    return _hp;
+        //}
+        //
+        //public void SetHp(float value) 
+        //{
+        //    if (value < 0)
+        //        value = 0;
+        //
+        //    _hp = value;
+        //}
+        //public void Damage(float amount)
+        //{
+        //    hp -= amount;
+        //}
+        public void Damage(float amount)
+        {
+            hp -= amount; // == hp = hp - amount;
+        }
+        public abstract void UniqueSkill(); // 추상함수는 정의하지않고 선언만 함.
+    }
+}
