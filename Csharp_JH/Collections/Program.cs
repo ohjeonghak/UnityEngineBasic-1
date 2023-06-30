0adcf9830e85c9f35240df51fa4e286746a3e3a4
ohjeonghak <136331677+ohjeonghak@users.noreply.github.com>
2023-06-30 21:51:07 +0900

    DynamicArray2
    06.30
    
    DynamicArray 2
    
    IEnumerator(Interface-IEnumerable)

--- Csharp_JH/Collections/Program.cs
+++ Csharp_JH/Collections/Program.cs
@@ -1,10 +1,54 @@
-﻿namespace Collections
+﻿using System.Collections;
+using System.Collections.Generic; // google 검색해보셈
+
+
+namespace Collections
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Queue<int> queue1 = new Queue<int>();
+            queue1.Enqueue(3);
+            queue1.Enqueue(2);
+            queue1.Enqueue(5);
+
+            while (queue1.Count > 0)
+            {
+                if (queue1.Peek() > 1)
+                Console.WriteLine(queue1.Dequeue());
+            }
+
+            Stack<float> stack1 = new Stack<float>();
+            stack1.Push(3);
+            stack1.Push(2);
+            stack1.Push(5);
+
+            while (stack1.Count > 0)
+            {
+                if (stack1.Peek() > 1)
+                    Console.WriteLine(stack1.Pop());
+            }
+
+            HashSet<int> visited = new HashSet<int>();
+            if (visited.Add(3))
+            { 
+                Console.WriteLine("Added 3 in hashet");
+            }
+            if (visited.Remove(2))
+            {
+                Console.WriteLine("Removed 2 in hashset");
+            }
+
+            MyDynamicArray myDynamicArray = new MyDynamicArray();
+            IEnumerator e1 = myDynamicArray.GetEnumerator();
+            Console.WriteLine(e1.Current);
+            e1.MoveNext();
+            Console.WriteLine(e1.Current);
+            e1.MoveNext();
+            Console.WriteLine(e1.Current);
+            e1.MoveNext();
+            Console.WriteLine(e1.Current);
         }
     }
 }
