b34428d6c677bdad35bc0b346f85802a17af666a
ohjeonghak <136331677+ohjeonghak@users.noreply.github.com>
2023-07-18 21:59:56 +0900

    AVTree3.cs
    07.18
    
    AVTree3
    
    Add, Contains, Remove

--- Csharp_JH/Collections/AVLTreeOfT.cs
+++ Csharp_JH/Collections/AVLTreeOfT.cs
@@ -20,6 +20,25 @@ namespace Collections
 
         private Node _root;
 
+
+
+        public bool Contains(T value)
+        {
+            Node node = _root;
+            while (node != null)
+            {
+                if (value.CompareTo(node.Value) < 0)
+                    node = node.Left;
+                else if (value.CompareTo(node.Value) > 0)
+                    node = node.Right;
+                else
+                    return true;
+            }
+            return false;   
+        } 
+
+
+
         public void Add(T value)
         {
             _root = Add(_root, value);
@@ -44,17 +63,102 @@ namespace Collections
 
             // ?? -> Null 병합연산자
             // null 이 아닐경우 왼쪽 값 반환, null 이면 오른쪽 값 반환
-            node.Height = 1 + Math.Max(node?.Left.Height ?? 0, node.Right.Height);
+            node.Height = 1 + Math.Max(node?.Left?.Height ?? 0, node.Right?.Height);
             int balance = Balance(node);
             // 왼쪽으로 치우쳐져있으면
             if (balance > 1)
             {
+                if (value.CompareTo(node.Left.Left) > 0)
+                {
+                    node.Left =  RotateLeft(node.Left);
+                }
 
+                return RotateLeft(node);
             }
+
+           
             //오른쪽으로 치우쳐져있으면
             else if (balance < -1)
             {
+                if (value.CompareTo(node.Right.Value) < 0)
+                {
+                    node.Right = RotateRight(node.Right);
+                }
 
+                return RotateRight(node);
+            }
+
+        }
+        public void Remove(T value)
+        {
+            _root = Remove(_root, value);
+        }
+
+
+        private Node Remove(Node node, T value)
+        {
+            if (node == null)
+                return null;
+
+            int compare = value.CompareTo(node.Value);
+            if (compare < 0)
+            {
+                node.Left = Remove(node.Left, value);
+            }
+            else if (compare > 0)
+            {
+                node.Right = Remove(node.Right, value);
+            }
+            else
+            {
+                //자식이 하나이던지 없던지 체크
+                if(node.Left == null)
+                {
+                    return node.Right;
+                }
+                else if (node.Right == null)
+                {
+                    return node.Left;
+                }
+                else
+                {
+                    Node tmp = node.Right;
+                    Node tmpParent = tmp;
+                    while (tmp.Left != null)
+                    {
+                        tmp = tmp.Left;
+                        tmpParent = tmp;
+                    }
+                    node.Value = tmp.Value;
+                    tmpParent.Left = null;
+                }
+            }
+
+            node.Height = 1 + Math.Max(node?.Left.Height ?? 0, node?.Right.Height ?? 0);
+
+            int balance = Balance(node);
+
+            // 왼쪽으로 치우쳐져있으면
+            if (balance > 1)
+            {
+                if (value.CompareTo(node.Left.Left) > 0)
+                {
+                    node.Left = RotateLeft(node.Left);
+                }
+
+                return RotateLeft(node);
+            }
+
+
+            //오른쪽으로 치우쳐져있으면
+            else if (balance < -1)
+            {
+                if (value.CompareTo(node.Right.Value) < 0)
+                {
+                    node.Right = RotateRight(node.Right);
+                }
+
+                return RotateRight(node);
             }
 
         }
@@ -66,8 +170,29 @@ namespace Collections
         /// <returns> 왼쪽 : > 1, 오른쪽 : < -1   </returns>
         private int Balance(Node node)
         {
-            return node != null ? (node?.Left.Height ?? 0 - node?.Right.Height ?? 0) : 0;
+            return node != null ? (node?.Left?.Height ?? 0 - node?.Right?.Height ?? 0) : 0;
+        }
+
+        private Node RotateLeft(Node node)
+        {
+            Node newRoot = node.Right;
+            node.Right = newRoot.Left;
+            newRoot.Left = node;
+
+            node.Height = 1 + Math.Max(node?.Left?.Height ?? 0, node?.Right?.Height ?? 0);
+            newRoot.Height = 1 + Math.Max(newRoot?.Left?.Height ?? 0, newRoot?.Right?.Height ?? 0);
+            return newRoot;
+        }
+
+        private Node RotateRight(Node node)
+        {
+            Node newRoot = node.Left;
+            node.Left = newRoot.Right;
+            newRoot.Right = node;
+
+            node.Height = 1 + Math.Max(node?.Left?.Height ?? 0, node?.Right?.Height ?? 0);
+            newRoot.Height = 1 + Math.Max(newRoot?.Left?.Height ?? 0, newRoot?.Right?.Height ?? 0);
+            return newRoot;
         }
     }           
 }
-
