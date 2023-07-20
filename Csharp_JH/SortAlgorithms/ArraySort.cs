d6fda60230a4566f9c690ecaedc1aa6cdcf690d3
ohjeonghak <136331677+ohjeonghak@users.noreply.github.com>
2023-07-20 21:51:24 +0900

    ArraySort2
    07.20
    
    ArraySort2
    
    SelecionSort , InsertionSort

--- Csharp_JH/SortAlgorithms/ArraySort.cs
+++ Csharp_JH/SortAlgorithms/ArraySort.cs
@@ -9,6 +9,11 @@ namespace SortAlgorithms
 {
     internal static class ArraySort
     {
+        /// <summary>
+        /// 거품 정렬
+        /// 시간복잡도는 O(N^2) , 연산이 계속 되기때문에 느림
+        /// </summary>
+        /// <param name="arr"></param>
         public static void BubbleSort(int[] arr)
         {
             int i, j;
@@ -23,6 +28,51 @@ namespace SortAlgorithms
                 }
             }
         }
+        /// <summary>
+        /// 선택 정렬
+        /// 시간복잡도는 O(N^2)
+        /// </summary>
+        /// <param name="arr"></param>
+        public static void SelecionSort(int[] arr)
+        {
+            int i, j, minIdx;
+            for (i = 0; i < arr.Length; i++)
+            {
+                minIdx = i;
+                for (j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < arr[minIdx])
+                        minIdx= j;
+                }
+
+                Swap(ref arr[i], ref arr[minIdx]);
+            }
+        }
+
+        /// <summary>
+        /// 삽입 정렬
+        /// 시간복잡도는 O(N^2)
+        /// stable
+        /// </summary>
+        /// <param name="arr"></param>
+        public static void InsertionSort(int[] arr)
+        {
+            int i, j;
+            int key;
+
+            for (i = 0; i < arr.Length; i++)
+            {
+                key = arr[i];
+                j = i - 1;
+                while (j >= 0 && arr[j] > key)
+                {
+                    arr[j + 1] = arr[j];
+                    j--;
+                }
+                arr[j + 1] = key;
+            }
+        }
+
         // ref : 인자를 변수의 참조로 받아야할떄 사용하는 키워드
         public static void Swap(ref int a, ref int b)
         {
--- Csharp_JH/SortAlgorithms/Program.cs
+++ Csharp_JH/SortAlgorithms/Program.cs
@@ -1,18 +1,36 @@
-﻿namespace SortAlgorithms
+﻿using System.Globalization;
+using System.Diagnostics;
+using System.Linq;
+namespace SortAlgorithms
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[] arr = { 1, 4, 3, 3, 9, 8, 7, 2, 5, 0 };
+
+            Random random = new Random();
+            int[] arr // = { 1, 4, 3, 3, 9, 8, 7, 2, 5, 0 };
+                = Enumerable
+                .Repeat(0, 100000)
+                .Select(i => random.Next(0, 100000))
+                .ToArray();
 
 
-        ArraySort.BubbleSort(arr);
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            //ArraySort.BubbleSort(arr);
+            //ArraySort.SelecionSort(arr);
+            ArraySort.InsertionSort(arr);
+            
+            stopwatch.Stop();
+            Console.WriteLine($"소요시간 : {stopwatch.ElapsedMilliseconds}");
 
-            for (int i = 0; i < arr.Length; i++)
-            {
-                Console.WriteLine($"{arr[i], })
-            }
+            //Console.Write("Result : ");
+            //
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    Console.Write($"{arr[i]},");
+            //}
         }
     }
 }
