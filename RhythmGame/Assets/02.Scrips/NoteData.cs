c4df9dfac6e65bfabe2b12008e874bba805064cb
ohjeonghak <136331677+ohjeonghak@users.noreply.github.com>
2023-08-14 22:02:22 +0900

    RhythmGame
    08.14
    
    RhythmGame
    
    Firstclass
    MusicvideoPlay, StartButton, Stopbutton, VideoPlayer

---
+++ RhythmGame/Assets/02.Scrips/NoteData.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public struct NoteData
+{
+    public KeyCode key;
+    public float time;
+}
+   
+
