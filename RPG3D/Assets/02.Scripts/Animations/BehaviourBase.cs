c75187dfd4a9aaa463f0321f3c5426efec3ad752
ohjeonghak <136331677+ohjeonghak@users.noreply.github.com>
2023-10-09 11:58:46 +0900

    RPG3D Game4
    10.09
    
    Animator ( Parameters - V , H , ComboStack )
    
    - Creat Animations
    ( Strafe, Run, Attack)
    ( TwoHand Starfa, TwoHand Run, TwoHand Attack)

--- RPG3D/Assets/02.Scripts/Animations/BehaviourBase.cs
+++ RPG3D/Assets/02.Scripts/Animations/BehaviourBase.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BehaviourBase : StateMachineBehaviour
-{ 
+{
+    public override void OnStateMachineEnter(Animator animator, int stateMachinePathHash)
+    {
+        base.OnStateMachineEnter(animator, stateMachinePathHash);
+    }
 
+    public override void OnStateMachineExit(Animator animator, int stateMachinePathHash)
+    {
+        base.OnStateMachineExit(animator, stateMachinePathHash);
+    }
 }
