de57450963f2b92e978816ca88b994d0fc107208
ohjeonghak <136331677+ohjeonghak@users.noreply.github.com>
2023-10-13 21:53:12 +0900

    RPG3D Game8
    10.13
    
    Scripts ( CharacterController - Ground <Check>
    -> _groundMask , _slope, _groundDetectRadius, _rigidbody, _inertia
    -> Gizmos

--- RPG3D/Assets/02.Scripts/Animations/CharacterController.cs
+++ RPG3D/Assets/02.Scripts/Animations/CharacterController.cs
@@ -1,5 +1,7 @@
 using System;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngineInternal;
 
 public enum State
 {
@@ -43,8 +45,16 @@ public abstract class CharacterController : MonoBehaviour
     public int comboMax;
     public int comboStack;
 
+    [SerializeField] private float _groundDetectRadius;
+    private Vector3 _inertia;
+    [SerializeField] private LayerMask _groundMask;
+    [SerializeField] private float _slope = 45.0f;
+    private Rigidbody _rigidbody;
+
+
     private void Awake()
     {
+        _rigidbody = GetComponent<Rigidbody>();
         _animator = GetComponent<Animator>();
         BehaviourBase[] behaviours = _animator.GetBehaviours<BehaviourBase>();
         for (int i = 0; i < behaviours.Length; i++)
@@ -83,8 +93,50 @@ public abstract class CharacterController : MonoBehaviour
 
     private void FixedUpdate()
     {
-        transform.localPosition += move * Time.fixedDeltaTime;
+        if (DetectGround())
+        {
+           _inertia.y = 0.0f;
+        }
+        else
+        {
+            _inertia.y += Physics.gravity.y * Time.fixedDeltaTime;
+        }
+        
+        if (_inertia.magnitude > 0.0f)
+            transform.Translate(_inertia * Time.fixedDeltaTime);
+        
+        Vector3 expected = transform.position
+                            + Quaternion.LookRotation(transform.forward, Vector3.up) * move * Time.deltaTime;
+
+        float distanceExpected = Vector3.Distance(transform.position, expected);
+
+        Debug.DrawRay(expected + Vector3.up,
+                      Vector3.down * (1.0f + Mathf.Tan(_slope) * distanceExpected),
+                      Color.red);
+        
+        if (Physics.Raycast(expected + Vector3.up,
+                        Vector3.down,
+                        out RaycastHit hit, 
+                        1.0f + Mathf.Tan(_slope) * distanceExpected,
+                        _groundMask))
+        {
+            // 45도 슬로프 검증
+            if (hit.point.y < transform.position.y + MathF.Tan(_slope) * distanceExpected)
+            {
+                expected = new Vector3(expected.x, hit.point.y, expected.z);
+                transform.position = expected;
+                
+            }
+        }
     }
+
+    private bool DetectGround()
+    {
+        Collider[] cols
+            = Physics.OverlapSphere(transform.position, _groundDetectRadius, _groundMask);
+        return cols.Length > 0;
+    }
+
 
     protected void ChangeState(State newState)
     {
@@ -163,4 +215,11 @@ public abstract class CharacterController : MonoBehaviour
 
         
     }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawWireSphere(transform.position, _groundDetectRadius);
+    }
+
+
 }
