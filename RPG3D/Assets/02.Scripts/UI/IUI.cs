3a537b25e6e2424e03133a239e9443714deef56a
ohjeonghak <136331677+ohjeonghak@users.noreply.github.com>
2023-10-30 21:43:52 +0900

    RPG3D Game18
    10.30
    
    UI Scripts Add
    -IUI -> inputActionEnabled
    -UIManager -> Get<T> , Push, Pop, HideLast
    -UIMonoBehaviour -> IUI interface, Awake

--- RPG3D/Assets/02.Scripts/UI/IUI.cs
+++ RPG3D/Assets/02.Scripts/UI/IUI.cs
@@ -8,12 +8,13 @@ namespace RPG.UI
     public interface IUI
     {
         int sortOrder { get; set; }
+        bool inputActionEnabled { get; set; }
 
         void Show();
         void Hide();
 
         event Action onShow;
         event Action onHide;
-
-    }   
+    }
+    
 }
